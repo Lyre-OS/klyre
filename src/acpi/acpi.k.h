89af398d0049ae044586fa13747b690b1682a9de
mintsuki <mintsuki@protonmail.com>
2024-02-20 09:39:31 +0100

    acpi: Fix definition of 'sdt' struct

--- src/acpi/acpi.k.h
+++ src/acpi/acpi.k.h
@@ -10,7 +10,7 @@ struct sdt {
     uint8_t revision;
     uint8_t checksum;
     char oem_id[6];
-    char oem_table_id[6];
+    char oem_table_id[8];
     uint32_t oem_revision;
     uint32_t creator_id;
     uint32_t creator_revision;
